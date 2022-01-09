7b7ffb1b77adff03b8c813e1fbff371260cbce39
JuanP Barba <juanbarba@sprinter.es>
2022-01-09 21:35:40 +0000

    refactor: define model
    Pending destroy and update methods.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,235 @@
-import { test, assert } from "vitest"
-import { foo } from "../src"
+import { join, dirname } from "path"
+import { mkdirSync, existsSync, unlinkSync } from "fs"
+import { describe, it, expect, afterEach, beforeEach } from "vitest"
+import { LowSync, MemorySync, JSONFileSync } from "lowdb"
+import { defineModel } from "../src"
 
-test("simple", () => {
-  assert.equal(foo, "foo")
+type User = {
+  id: string
+  name: string
+  surname: string
+  roles?: {
+    name: string
+  }[]
+}
+const data = (): User[] => [
+  {
+    id: "1",
+    name: "John",
+    surname: "Doe",
+    roles: [{ name: "admin" }],
+  },
+  {
+    id: "2",
+    name: "Jane",
+    surname: "Doe",
+    roles: [{ name: "admin" }],
+  },
+]
+
+describe("find", () => {
+  it("returns value by id", () => {
+    const UserModel = createInMemoryUserModel()
+    const id = data()[0].id
+    const matched = UserModel.find({ id })
+    expect(matched?.valueOf()).toEqual(data()[0])
+  })
+  it("returns undefined", () => {
+    const UserModel = createInMemoryUserModel()
+    const id = "non"
+    expect(UserModel.find({ id })).toBeUndefined()
+  })
 })
+
+describe("all", () => {
+  it("returns all values in db", () => {
+    const UserModel = createInMemoryUserModel()
+    expect(UserModel.all().map((i) => i.valueOf())).toEqual(data())
+  })
+  it("returns empty list", () => {
+    const UserModel = createInMemoryUserModel(() => [] as any)
+    expect(UserModel.all()).toEqual([])
+  })
+})
+
+describe("filter", () => {
+  it("returns filtered values in db", () => {
+    const filters = { name: "John" }
+    const UserModel = createInMemoryUserModel()
+    expect(UserModel.filter(filters).length).toEqual(1)
+  })
+  it("returns empty list", () => {
+    const filters = { surname: "Doce" }
+    const UserModel = createInMemoryUserModel()
+    expect(UserModel.filter(filters).length).toEqual(0)
+  })
+})
+
+describe("create", () => {
+  it("returns non persisted data model", () => {
+    const user = { id: "3", name: "Jr", surname: "Doe" }
+    const UserModel = createInMemoryUserModel()
+
+    expect(UserModel.create(user).valueOf()).toEqual(user)
+    expect(UserModel.find(user)).toBeUndefined()
+  })
+})
+
+describe("save", () => {
+  it("returns persisted data", () => {
+    const givenUser = { id: "3", name: "Jr", surname: "Doe" }
+    const UserModel = createInMemoryUserModel()
+    const userModelData = UserModel.create(givenUser)
+    userModelData.save()
+
+    expect(UserModel.find(givenUser)?.valueOf()).toEqual(givenUser)
+  })
+})
+
+describe("first", () => {
+  it("returns first value", () => {
+    const UserModel = createInMemoryUserModel()
+    const userData = UserModel.first()
+
+    const expectedData = data()[0]
+    expect(userData?.valueOf()).toEqual(expectedData)
+  })
+  it("returns undefined", () => {
+    const UserModel = createInMemoryUserModel(() => [] as any)
+    expect(UserModel.first()).toBeUndefined()
+  })
+})
+
+describe("last", () => {
+  it("returns last value", () => {
+    const UserModel = createInMemoryUserModel()
+    const userData = UserModel.last()
+
+    const expectedData = data()[1]
+    expect(userData?.valueOf()).toEqual(expectedData)
+  })
+  it("returns undefined", () => {
+    const UserModel = createInMemoryUserModel(() => [] as any)
+    expect(UserModel.first()).toBeUndefined()
+  })
+})
+
+describe("get", () => {
+  it("returns value under path", () => {
+    const UserModel = createInMemoryUserModel()
+    const name = UserModel.first()?.get("name")
+
+    const expectedName = data()[0].name
+    expect(name).toEqual(expectedName)
+  })
+  it("returns nested value under path", () => {
+    const UserModel = createInMemoryUserModel()
+    const roleName = UserModel.first()?.get("roles.0.name")
+
+    const expectedRoleName = data()[0].roles![0].name
+    expect(roleName).toEqual(expectedRoleName)
+  })
+})
+
+describe("set", () => {
+  it("sets data to given path", () => {
+    const givenSurname = "Smith"
+    const UserModel = createInMemoryUserModel()
+    const user = UserModel.first()!
+    user.set("surname", givenSurname)
+    expect(user.get("surname")).toEqual(givenSurname)
+  })
+
+  it("sets nested value under path", () => {
+    const givenRoleName = "owner"
+    const UserModel = createInMemoryUserModel()
+    const user = UserModel.first()!
+    user.set("roles.0.name", givenRoleName)
+    expect(user.get("roles.0.name")).toEqual(givenRoleName)
+  })
+})
+
+function createInMemoryUserModel(seeds = data) {
+  const adapter = new MemorySync<User[]>()
+  const db = new LowSync<User[]>(adapter)
+  return defineModel<User>({
+    db,
+    seeds,
+  })
+}
+
+describe("save", () => {
+  const file = join(__dirname, ".temp/users.db.json")
+  prepareDb(file)
+
+  it("returns persisted data", () => {
+    const givenUser = { id: "3", name: "Jr", surname: "Doe" }
+    const UserModel = createInFileUserModel(file)
+    const userModelData = UserModel.create(givenUser)
+    userModelData.save()
+
+    expect(createInFileUserModel(file).last()?.valueOf()).toEqual(givenUser)
+  })
+
+  it("returns updated data", () => {
+    const givenSurname = "Smith"
+    const UserModel = createInFileUserModel(file)
+    const user = UserModel.first()!
+    user.set("surname", givenSurname)
+    user.save()
+
+    expect(createInFileUserModel(file).first()?.valueOf().surname).toEqual(
+      givenSurname,
+    )
+  })
+})
+
+describe("add", () => {
+  const file = join(__dirname, ".temp/users.db.json")
+  prepareDb(file)
+
+  it("creates and saves user", () => {
+    const givenUser = { id: "3", name: "Jr", surname: "Doe" }
+    const UserModel = createInFileUserModel(file)
+    UserModel.add(givenUser)
+    expect(createInFileUserModel(file).last()?.valueOf()).toEqual(givenUser)
+  })
+})
+
+describe("reload", () => {
+  const file = join(__dirname, ".temp/users.db.json")
+  prepareDb(file)
+
+  it("reloads db instance data", () => {
+    const givenDbConnection = createInFileUserModel(file)
+    expect(givenDbConnection.data).toEqual(data())
+
+    const givenUser = { id: "3", name: "Jr", surname: "Doe" }
+    const UserModel = createInFileUserModel(file)
+    UserModel.add(givenUser)
+    expect(givenDbConnection.last()?.valueOf()).not.toEqual(givenUser)
+
+    givenDbConnection.reload()
+    expect(givenDbConnection.last()?.valueOf()).toEqual(givenUser)
+  })
+})
+
+function prepareDb(file: string) {
+  beforeEach(() => {
+    const dir = dirname(file)
+    dir && !existsSync(dir) && mkdirSync(dir)
+  })
+
+  afterEach(() => {
+    if (existsSync(file)) unlinkSync(file)
+  })
+}
+
+function createInFileUserModel(file: string, seeds = data) {
+  const adapter = new JSONFileSync<User[]>(file)
+  const db = new LowSync<User[]>(adapter)
+  return defineModel<User>({
+    db,
+    seeds,
+  })
+}
